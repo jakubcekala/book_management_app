37ddc15f3cbea446d031aefdc991bb25890ccf83
jcekala <jakubcekala1@gmail.com>
2021-11-28 20:45:51 +0100

    Edit about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="home">
+    <h1 class="subheading black--text pa-5">Project repository and full instruction can be found here: </h1>
+    <a class="black--text pa-5" href="https://github.com/jakubcekala/book_management_app">https://github.com/jakubcekala/book_management_app</a>
   </div>
 </template>
