d54ed278633bfd4cf32c7f30aa67a875d24ec8a6
jcekala <jakubcekala1@gmail.com>
2021-11-28 11:07:43 +0100

    Create Appbar

---
+++ src/components/Appbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <v-toolbar app class="text-uppercase black--text">
+      <v-toolbar-title>
+        <span>Books List</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <span>Jakub Cekała (277391)</span>
+    </v-toolbar>
+
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
